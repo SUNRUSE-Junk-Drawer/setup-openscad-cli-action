01dd60450d66ece916cf415e5ef0c9e25a9dc4af
jameswilddev <jameswilddev@outlook.com>
2021-03-06 20:50:58 +0000

    Attempt to fix permissions in CI.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 set -e
 
-sudo echo "xvfb-run $PWD/submodules/openscad/openscad/openscad \$@" > /usr/local/bin/openscad
+sudo bash -c 'echo "xvfb-run $PWD/submodules/openscad/openscad/openscad \$@" > /usr/local/bin/openscad'
 
 sudo chmod +x /usr/local/bin/openscad
