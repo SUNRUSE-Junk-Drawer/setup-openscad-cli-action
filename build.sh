38a5baa007862c807ea14324bd1114ab25a0f330
jameswilddev <jameswilddev@outlook.com>
2021-03-07 08:13:45 +0000

    Tune to hopefully increase build performance without OOM risk.

--- build.sh
+++ build.sh
@@ -11,18 +11,18 @@ if [ "$(uname)" == "Darwin" ]; then
   source setenv_mac.sh
   ./scripts/macosx-build-dependencies.sh
   qmake openscad.pro
-  make -j 2
+  make -j 6
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   sudo apt-get install libglew-dev libxmu-dev libxi-dev qt5-default qtmultimedia5-dev libboost-filesystem-dev libeigen3-dev libmpc-dev libqscintilla2-qt5-dev libcgal-dev libharfbuzz-dev libopencsg-dev libboost-regex-dev gettext itstool -y
   source ./scripts/setenv-unibuild.sh
   ./scripts/check-dependencies.sh
   qmake openscad.pro
-  make -j 2
+  make -j 4
 else
   source ./scripts/setenv-mingw-xbuild.sh 64
   ./scripts/mingw-x-build-dependencies.sh 64
   ./scripts/release-common.sh mingw64
   cd mingw64
   qmake ../openscad.pro CONFIG+=mingw-cross-env
-  make -j 2
+  make -j 4
 fi
