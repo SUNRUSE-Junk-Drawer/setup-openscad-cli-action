3eda462c530f5a2e9dd3f4d9208a93070b6fe1ec
jameswilddev <jameswilddev@outlook.com>
2021-03-07 07:28:03 +0000

    Allow parallel test runs.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 set -e
 
-sudo bash -c 'echo "xvfb-run $PWD/clone/submodules/openscad/openscad/openscad \$@" > /usr/local/bin/openscad'
+sudo bash -c 'echo "xvfb-run -a $PWD/clone/submodules/openscad/openscad/openscad \$@" > /usr/local/bin/openscad'
 
 sudo chmod +x /usr/local/bin/openscad
