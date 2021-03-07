1695891931da7fa9a9420851378cd908dfd0a34c
jameswilddev <jameswilddev@outlook.com>
2021-03-07 17:24:17 +0000

    Debug.

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 set -e
 
 if [ "$(uname)" == "Darwin" ]; then
+  find $PWD/clone/submodules/openscad/openscad
   sudo ln -s $PWD/clone/submodules/openscad/openscad/openscad /usr/local/bin/openscad
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   sudo bash -c 'echo "xvfb-run -a $PWD/clone/submodules/openscad/openscad/openscad \$@" > /usr/local/bin/openscad'
