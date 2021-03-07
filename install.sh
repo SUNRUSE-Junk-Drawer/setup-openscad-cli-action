1f6af42d7e20887934cd02792082652b9c41ae6d
jameswilddev <jameswilddev@outlook.com>
2021-03-07 17:57:50 +0000

    Fix MacOS path.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ set -e
 
 if [ "$(uname)" == "Darwin" ]; then
   find $PWD/clone/submodules/openscad/openscad
-  sudo ln -s $PWD/clone/submodules/openscad/openscad/openscad /usr/local/bin/openscad
+  sudo ln -s $PWD/clone/submodules/openscad/openscad/OpenSCAD.app/Contents/MacOS/OpenSCAD /usr/local/bin/openscad
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   sudo bash -c 'echo "xvfb-run -a $PWD/clone/submodules/openscad/openscad/openscad \$@" > /usr/local/bin/openscad'
   sudo chmod +x /usr/local/bin/openscad
